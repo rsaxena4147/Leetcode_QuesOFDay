7ddb3cd0d1b16e14be040f57bbb47788da986100
Rimjhim <rimjhimsaxena535@gmail.com>
2025-09-02 15:10:14 +0530

    day23

--- LC1792.cpp
+++ LC1792.cpp
@@ -2,42 +2,49 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    auto gain(int a , int b){
-        return (double)(a+1)/(b+1) - (double)a/b;
+  auto gain(int a, int b)
+  {
+    return (double)(a + 1) / (b + 1) - (double)a / b;
+  }
+  double maxAverageRatio(vector<vector<int>> &classes, int extraStudents)
+  {
+
+    priority_queue<pair<double, pair<int, int>>> pq;
+
+    for (int i = 0; i < classes.size(); i++)
+    {
+
+      int a = classes[i][0];
+      int b = classes[i][1];
+
+      pq.push({gain(a, b), {a, b}});
     }
-    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
 
-        priority_queue<pair<double,pair<int,int>>> pq;
+    while (extraStudents-- > 0)
+    {
+     
+      auto [gain_val, ab] = pq.top();
+      pq.pop();
+      int f = ab.first;
+      int s = ab.second;
 
-        for(int i = 0 ; i < classes.size() ; i++){
+      f++;
+      s++;
 
-            int a = classes[i][0];
-            int b = classes[i][1];
-
-            pq.push({gain(a,b) , {a,b}});
-        }
-
-        while(extraStudents-- > 0){
-            auto [gain_val, ab] = pq.top();
-            pq.pop();
-            int f = ab.first;
-            int s = ab.second;
-
-            f++;
-            s++;
-
-            pq.push({gain(f,s) , {f,s}});
-        }
-
-        double sum = 0.0;
-        while (!pq.empty()) {
-            auto [g, ab] = pq.top(); pq.pop();
-            sum += (double)ab.first / ab.second;
-        }
-
-        return sum / classes.size();
-        
+      pq.push({gain(f, s), {f, s}});
     }
+
+    double sum = 0.0;
+    while (!pq.empty())
+    {
+      auto [g, ab] = pq.top();
+      pq.pop();
+      sum += (double)ab.first / ab.second;
+    }
+
+    return sum / classes.size();
+  }
 };
