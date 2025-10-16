b37b76c6d7de72b54ca1daf6a761cdcc80032afe
Rimjhim <rimjhimsaxena535@gmail.com>
2025-10-16 22:13:30 +0530

    day**

--- LC778.cpp
+++ LC778.cpp
@@ -18,7 +18,7 @@ public:
         while (!pq.empty()) {
             auto t = pq.top(); pq.pop();
             int           
-            time = t[0], x = t[1], y = t[2];
+            time              = t[0], x = t[1], y = t[2];
 
             if (x == n - 1 && y == n - 1)
                 return time;
