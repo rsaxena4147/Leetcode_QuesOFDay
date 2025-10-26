1dd0214bd012339ac74fcc7383c18c6f5b44f417
Rimjhim <rimjhimsaxena535@gmail.com>
2025-10-26 18:42:05 +0530

    day56

---
+++ LC2043.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+class Bank {
+public:
+    vector<long long> balance;
+    int n;
+    Bank(vector<long long>& balance) {
+        this->balance = balance;
+        this->n = balance.size();
+    }
+
+    bool transfer(int account1, int account2, long long money) {
+        if (account1 - 1 >= n or account2 - 1 >= n) {
+            return false;
+        }
+        if (money <= balance[account1 - 1]) {
+            balance[account1 - 1] -= money;
+            balance[account2 - 1] += money;
+            return true;
+        }
+        return false;
+    }
+
+    bool deposit(int account, long long money) {
+        if (account - 1 >= n) {
+            return false;
+        }
+
+        balance[account - 1] += money;
+
+        return true;
+    }
+
+    bool withdraw(int account, long long money) {
+
+        if (account - 1 >= n) {
+            return false;
+        }
+
+        if (money <= balance[account - 1]) {
+            balance[account - 1] -= money;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
