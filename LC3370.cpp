07bcc22bcf6f9802b6c4679b3fc19a029f983e5e
Rimjhim <rimjhimsaxena535@gmail.com>
2025-10-29 09:00:30 +0530

    day57

---
+++ LC3370.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool solve(int i){
+
+        while(i > 1){
+         
+            if(i%2 == 0){
+                return false;
+            }
+            i = i / 2;
+        }
+        return true;
+    }
+    int smallestNumber(int n) {
+
+        int i = n;
+
+        while(i >= n){
+           if(solve(i)){
+            return i;
+           }
+           i++;
+        }
+        return -1;
+    }
+};
