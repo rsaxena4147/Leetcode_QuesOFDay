062785b37d1514deb92ed1b54d6099789cfae66a
Rimjhim <rimjhimsaxena535@gmail.com>
2025-11-09 10:02:17 +0530

    day68

---
+++ LC2169.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int count = 0;
+        if(num1 == 0 or num2 == 0){
+                return 0;
+            }
+
+        while(num1 != 0 or num2 != 0){
+            if(num1 > num2){
+                num1 -= num2;
+            }else{
+                num2 -= num1;
+            }
+
+            count++;
+            if(num1 == 0 or num2 == 0){
+                break;
+            }
+        }
+        return count;
+    }
+};
